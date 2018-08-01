62585671b3e66dc493b9970898b64942d7c7b10a
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-08-01 17:07:23 +1000

    train fix

--- src/components/WindowPicker/index.jsx
+++ src/components/WindowPicker/index.jsx
@@ -22,7 +22,7 @@ class WindowPicker extends React.Component {
         const buttons = options.values.map( value => (
         <Button key={value} 
                 value={value}
-                selected={ options == selectedOption }
+                selected={ value == selectedOption }
                 handleSelect={this.props.handleSelect}
                 />
         ))
