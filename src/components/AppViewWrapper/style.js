faa6761e33096a85222d7217f0f481a3964f0b71
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-08-04 09:51:48 +1000

    working fixed background height on window width scaling

--- src/components/AppViewWrapper/style.js
+++ src/components/AppViewWrapper/style.js
@@ -3,14 +3,12 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
 display: flex;
 min-height: 280px;
-width: 100%;
 justify-content: center;
 background-image: ${props => `url(${props.background})`};
 opacity: ${props => props.opacity};
 transition: ${props => `opacity ${props.duration}ms ease-in-out`};
 background-repeat: no-repeat;
+background-position: bottom;
 background-size: cover;
-height: 0;
-padding: 0; /* reset */
-padding-bottom: calc(100% * 600/ 1440);
+
 `
