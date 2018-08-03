9273e361489fb89b469059068f105e05b3bc98f2
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-08-03 19:30:43 +1000

    added skeleton components for ShortAlerts and TopShortList views

---
+++ src/components/ShortAlertsRow/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Renders a given row in the alert & anomalies widget.
+ */
+
+class ShortedAlertRow extends Reaxt.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<div>alert row goes here</div>)
+    }
+}
---
+++ src/components/TopShortsListRow/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Renders a specfiic row that is contained within the TopSHortList view. This will show
+ * the current short percentage of a given stock within the top x number of short positions
+ */
+
+class TopShortListRow extends Reaxt.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (<div>row goes here</div>)
+    }
+}
