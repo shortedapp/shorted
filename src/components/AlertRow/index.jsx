e2a5e4f04f74ea540a40d0b562fce3f1630ce0d6
Ben Ebsworth <ben.ebsworth@gmail.com>
2018-08-09 21:41:11 +1000

    added alert widget

--- src/components/AlertRow/index.jsx
+++ src/components/AlertRow/index.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-import { Wrapper , Name, Code } from './style';
+import {
+    Wrapper,
+    Name,
+    Code,
+    Percentage,
+    IndicatorWrapper,
+    IndicatorUp,
+    IndicatorDown,
+    PercentageChanged,
+    PercentageCurrent } from './style';
 
 /**
  * Renders a given row in the alert & anomalies widget.
@@ -9,6 +18,13 @@ const AlertRow = (props) =>  (
     <Wrapper>
         <Code><div className="code">{props.code}</div></Code>
         <Name>{props.name}</Name>
+        <Percentage>
+            <PercentageCurrent>{props.currentPercent}%</PercentageCurrent>
+            <PercentageChanged value={props.changedPercent} >{props.changedPercent}%</PercentageChanged>
+        </Percentage>
+        <IndicatorWrapper>
+        {props.changedPercent > 0 ? <IndicatorUp /> : <IndicatorDown/>}
+        </IndicatorWrapper>
     </Wrapper>
 )
 
