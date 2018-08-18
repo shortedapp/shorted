d5b2c2a397a8187d97939dbe094f0d637a2311d2
“Vaughan <vaughan.stedman@gmail.com>
2018-08-18 18:11:56 +1000

    Header styling changes

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -2,21 +2,18 @@ import styled from 'styled-components';
 
 export const duration = 500;
 export const transitionStyles = {
-  entering: { opacity: 0, Ypos: -100},
-  entered: { opacity: 1, Ypos: 0},
-  exited: { opacity: 0}
+  entering: {opacity: 0, Ypos: -100},
+  entered: {opacity: 1, Ypos: 0},
+  exited: {opacity: 0},
 };
 
 export const Wrapper = styled.div`
-    display: grid;
-    grid-gap: 20px;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-areas:
-        "logo navbar";
-    margin-right: 100px;
-    margin-left: 100px;
-    opacity: ${props => props.opacity};
-    transition: ${props => `${props.duration}ms ease-in-out`};
-    transition-property: opacity, transform;
-    transform: ${props => `translateY(${props.Ypos}px)`};
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  opacity: ${props => props.opacity};
+  transition: ${props => `${props.duration}ms ease-in-out`};
+  transition-property: opacity, transform;
+  transform: ${props => `translateY(${props.Ypos}px)`};
+`;
--- src/components/Logo/style.js
+++ src/components/Logo/style.js
@@ -1,23 +1,22 @@
 import styled from 'styled-components';
 
 export const LogoImageWrapper = styled.img`
-    fill: #e7e6ea;
-`
+  fill: #e7e6ea;
+`;
 export const Wrapper = styled.a`
-    display: flex;
-    float: left;
-    grid-area: logo;
-    margin-top: 20px;
-    text-decoration: none!important;
-    color: black;
-`
+  display: flex;
+  float: left;
+  margin-top: 20px;
+  text-decoration: none !important;
+  color: black;
+`;
 export const LogoTextWrapper = styled.span`
-    display: flex;
-    flex-direction: column-reverse;
-    font-size: 30px;
-    letter-spacing: 5px;
-    margin-left: 10px;
-    color: #e7e6ea;
-    font-weight: bold;
-    font-family: Avenir Next,sans-serif;
-`
+  display: flex;
+  flex-direction: column-reverse;
+  font-size: 30px;
+  letter-spacing: 5px;
+  margin-left: 10px;
+  color: #e7e6ea;
+  font-weight: bold;
+  font-family: Avenir Next, sans-serif;
+`;
