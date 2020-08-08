8c76b4b749ddaa95da6ace149a4229b92d246e4e
Ben Ebsworth <ben.ebsworth@gmail.com>
2020-08-08 12:08:27 +1000

    added function tf module

---
+++ infrastructure/modules/function/main.tf
@@ -0,0 +1,36 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "${var.configuration.name}-${random_id.suffix.hex}"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = var.configuration.zipPath
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = var.configuration.name
+  description = var.configuration.description
+  runtime     = var.configuration.runtime
+
+  available_memory_mb   = var.configuration.memory
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = var.configuration.triggerType == "http" ? true : false
+  entry_point           = var.configuration.entrypoint
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  count          = var.configuration.public ? 1 : 0
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
