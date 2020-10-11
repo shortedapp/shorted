8d7699d068a8b8e595d9c8989a45c6615df91c42
Ben Ebsworth <ben.ebsworth@gmail.com>
2020-10-11 23:33:36 +1100

    WIP: collector API for sync support

--- services/collector/internal/service/collector/collector.go
+++ services/collector/internal/service/collector/collector.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/shortedapp/shorted/services/collector/pkg/config"
 	"github.com/shortedapp/shorted/services/collector/pkg/log"
-	// v1 "github.com/shortedapp/shorted/shortedapis/pkg/collector/v1"
+	v1 "github.com/shortedapp/shorted/shortedapis/pkg/collector/v1"
 )
 
 type Service struct {
@@ -20,11 +20,6 @@ func New(ctx context.Context, cfg *config.Config) (*Service, error) {
 }
 
 // GetWatch information about a specific configured Watch
-// func (s *Service) GetSource(ctx context.Context, in *v1.Get) (*v1.GetWatcherResponse, error) {
-// 	watcher, err := w.store.Get(in.Id)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return &v1.GetWatcherResponse{
-// 		Watch: watcher}, nil
-// }
+func (s *Service) GetSource(ctx context.Context, in *v1.GetSourceRequest) (*v1.GetSourceResponse, error) {
+	return &v1.GetSourceResponse{}, nil
+}
