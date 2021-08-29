40fda1997d48cee8fcd1caa8c5d048303eb871b7
Ben Ebsworth <ben.ebsworth@gmail.com>
2021-08-29 10:23:02 +1000

    update go generated code :robot:

--- shortedapis/build.go
+++ shortedapis/build.go
@@ -1,7 +1,7 @@
 // Watcher
-//go:generate protoc ./proto/shorted/watcher/v1/watcher.proto -I proto/ -I third_party/ --gofast_out=plugins=grpc:pkg --validate_out=lang=go:pkg
-//go:generate protoc ./proto/shorted/watcher/v1/watcher_service.proto -I proto/ -I third_party/ --gofast_out=plugins=grpc:pkg --validate_out=lang=go:pkg --grpc-gateway_out=logtostderr=false:pkg --swagger_out=logtostderr=false:pkg
+//go:generate protoc ./proto/shorted/watcher/v1/watcher.proto -I proto/ -I third_party/ --go_out=plugins=grpc:pkg --validate_out=lang=go:pkg
+//go:generate protoc ./proto/shorted/watcher/v1/watcher_service.proto -I proto/ -I third_party/ --go_out=plugins=grpc:pkg --validate_out=lang=go:pkg --grpc-gateway_out=logtostderr=false:pkg --swagger_out=logtostderr=false:pkg
 // Collector 
-//go:generate protoc ./proto/shorted/collector/v1/collector.proto -I proto/ -I third_party/ --gofast_out=plugins=grpc:pkg --validate_out=lang=go:pkg
-//go:generate protoc ./proto/shorted/collector/v1/collector_service.proto -I proto/ -I third_party/ --gofast_out=plugins=grpc:pkg --validate_out=lang=go:pkg --grpc-gateway_out=logtostderr=false:pkg --swagger_out=logtostderr=false:pkg
+//go:generate protoc ./proto/shorted/collector/v1/collector.proto -I proto/ -I third_party/ --go_out=plugins=grpc:pkg --validate_out=lang=go:pkg
+//go:generate protoc ./proto/shorted/collector/v1/collector_service.proto -I proto/ -I third_party/ --go_out=plugins=grpc:pkg --validate_out=lang=go:pkg --grpc-gateway_out=logtostderr=false:pkg --swagger_out=logtostderr=false:pkg
 package shortedapis
