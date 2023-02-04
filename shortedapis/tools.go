bb1c632db96c89e74a40c5062364cdbd87ba365e
Ben Ebsworth <ben.ebsworth@gmail.com>
2023-02-04 22:04:15 +0800

    add new code gen

--- shortedapis/tools.go
+++ shortedapis/tools.go
@@ -4,11 +4,11 @@ package tools
 
 import (
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/mwitkow/go-proto-validators/protoc-gen-govalidators"
 	_ "github.com/rakyll/statik"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "github.com/envoyproxy/protoc-gen-validate"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+    _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 )
